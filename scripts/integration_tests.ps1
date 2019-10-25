325b651f6e295820a0b2c65361d640a42ada155a
Salim Afiune <afiune@chef.io>
2019-10-24 23:20:13 -0600

    Enable integration tests in Windows
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ scripts/integration_tests.ps1
@@ -0,0 +1,9 @@
+ECHO "-- Installing Golang"
+hab pkg install -b afiune/go
+
+ECHO "-- Building chef-analyze EXE"
+go build -o bin\chef-analyze_windows.exe
+
+ECHO "-- Running integration tests"
+$Env:CHEF_ANALYZE_BIN = (Get-PSDrive Habitat).Root + "\src\bin\chef-analyze_windows.exe"
+go test github.com/chef/chef-analyze/integration -v
