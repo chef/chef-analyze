406b2d53a16a37bd8f8d04910f83f8825fa34c7d
Salim Afiune <afiune@chef.io>
2019-10-25 10:09:23 -0600

    Add further -h/--help integration tests
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- scripts/integration_tests.ps1
+++ scripts/integration_tests.ps1
@@ -4,6 +4,6 @@ hab pkg install -b afiune/go
 ECHO "-- Building chef-analyze EXE"
 go build -o bin\chef-analyze_windows.exe
 
-ECHO "-- Running integration tests"
+ECHO "-- Running integration tests (github.com/chef/chef-analyze/integration)"
 $Env:CHEF_ANALYZE_BIN = (Get-PSDrive Habitat).Root + "\src\bin\chef-analyze_windows.exe"
 go test github.com/chef/chef-analyze/integration -v
