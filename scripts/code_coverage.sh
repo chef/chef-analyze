bdde2e666a806aee587bf5381d5b319125b96e56
Salim Afiune <afiune@chef.io>
2019-10-28 16:16:11 -0600

    Code Coverage script to comment back the results
    This automate will add a comment to every Pull Request open and display
    the details about the code coverage.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ scripts/code_coverage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# the generated code coverage profile coming from our habitat studio
+COVERAGE_TXT="results/coverage.txt"
+# extract the repository name from the environment variable BUILDKITE_REPO
+# example: "https://github.com/chef/chef-analyze.git"
+REPO_NAME=$(echo $BUILDKITE_REPO | cut -d/ -f5 | cut -d. -f1)
+# Github API to comment back to the open pull request
+COMMENTS_URL="https://api.github.com/repos/${BUILDKITE_ORGANIZATION_SLUG}/${REPO_NAME}/issues/${BUILDKITE_PULL_REQUEST}/comments"
+
+function post_data() {
+  cat <<EOF
+{"body":"# Code Coverage \\n$(echo `code_coverage_table`)"}
+EOF
+}
+
+function code_coverage_table() {
+  cat <<EOF
+\\nCode details | Function name | % Coverage
+\\n------------ | ------------- | ----------
+\\n
+EOF
+  awk '{ printf "%s | %s | %s \\n", $1, $2, $3 }' < $COVERAGE_TXT
+}
+
+hab studio run "source .studiorc && code_coverage"
+
+curl -H "Authorization: token $GITHUB_TOKEN" "$COMMENTS_URL" -d "$(post_data)"
