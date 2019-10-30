ef5f0b2577754fe67073f13870477ca75b47b538
Salim Afiune <afiune@chef.io>
2019-10-30 16:20:44 -0600

    Save CODE_COVERAGE for new PR comparison
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- .expeditor/update_coverage.sh
+++ .expeditor/update_coverage.sh
@@ -13,20 +13,23 @@ hab studio run "source .studiorc && code_coverage"
 aws --profile chef-cd s3 sync coverage "s3://chef-workstation-coverage/$BUILDKITE_PIPELINE_SLUG/current"
 aws --profile chef-cd s3 sync coverage "s3://chef-workstation-coverage/$BUILDKITE_PIPELINE_SLUG/$COVERAGE_DATE"
 
-COVERAGE=$(grep -w total: coverage/coverage.txt| awk '{print $NF}' | cut -d. -f1 | sed -e 's/%//')
+COVERAGE=$(grep -w total: coverage/coverage.txt| awk '{print $NF}' | sed -e 's/%//')
 
 case $COVERAGE in
   100) COLOR=brightgreen ;;
-  9[0-9]) COLOR=brightgreen ;;
-  8[0-9]) COLOR=green ;;
-  7[0-9]) COLOR=yellowgreen ;;
-  6[0-9]) COLOR=yellow ;;
-  5[0-9]) COLOR=orange ;;
+  9[0-9]*) COLOR=brightgreen ;;
+  8[0-9]*) COLOR=green ;;
+  7[0-9]*) COLOR=yellowgreen ;;
+  6[0-9]*) COLOR=yellow ;;
+  5[0-9]*) COLOR=orange ;;
   *) COLOR=red ;;
 esac
 
-sed -i -r "s|badge/coverage-[0-9]+\\%25-[a-z]+\\)|badge/coverage-${COVERAGE}\\%25-${COLOR}\\)|" README.md
-
+sed -i -r "s|badge/coverage-[0-9]+.[0-9]+\\%25-[a-z]+\\)|badge/coverage-${COVERAGE}\\%25-${COLOR}\\)|" README.md
 git add README.md
+
+echo "$COVERAGE" > CODE_COVERAGE
+git add CODE_COVERAGE
+
 git commit --message "Update code coverage to ${COVERAGE}%"
 git push origin master
