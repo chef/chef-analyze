cd9e99e822c612f80b9d9383fd26355767bfd1eb
Salim Afiune <afiune@chef.io>
2019-10-31 23:57:42 -0600

    Implement chaining actions when workflow completed
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- .expeditor/update_coverage.sh
+++ .expeditor/update_coverage.sh
@@ -2,18 +2,10 @@
 
 set -eou pipefail
 
-export COVERAGE_DATE
-COVERAGE_DATE="$(date +"%Y-%m-%dT%H:%M:%S")"
-
-
 COVERAGE_FOLDER=$(echo "$EXPEDITOR_SCM_SETTINGS_REPO" | sed -e 's/\//-/')
 COVERAGE_FOLDER="$COVERAGE_FOLDER-$EXPEDITOR_SCM_SETTINGS_BRANCH"
 
-# Generate code coverage to upload to S3 bucket and update to master
-hab studio run "source .studiorc && code_coverage"
-
-aws --profile chef-cd s3 sync coverage "s3://chef-workstation-coverage/$COVERAGE_FOLDER/current"
-aws --profile chef-cd s3 sync coverage "s3://chef-workstation-coverage/$COVERAGE_FOLDER/$COVERAGE_DATE"
+aws --profile chef-cd s3 cp "s3://chef-workstation-coverage/$COVERAGE_FOLDER/current" coverage
 
 COVERAGE=$(grep -w total: coverage/coverage.txt| awk '{print $NF}' | sed -e 's/%//')
 
@@ -34,4 +26,4 @@ echo "$COVERAGE" > CODE_COVERAGE
 git add CODE_COVERAGE
 
 git commit --message "Update code coverage to ${COVERAGE}%"
-git push origin master
+#git push origin master
