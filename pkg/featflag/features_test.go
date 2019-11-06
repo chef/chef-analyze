b8a968bfd2c0794dd75ef7507b56e2922151a320
Salim Afiune <afiune@chef.io>
2019-11-05 23:46:55 -0700

    Integrate Config with Feature Flags library
    This change is enabling the `featflag` go package to interact with a
    configuration file (`config.toml`).
    
    The `featflag` go library is an abstraction that manipulates feature
    flags through environment variables and a configuration file
    (`config.toml`), by default, it contains global flags that can be
    used in multiple go packages.
    
    **example 1**: use a global feature flag
    ```go
    if featflag.ChefFeatAnalyze.Enabled() {
      // the analyze feature is enabled, act upon it
    }
    ```
    
    **example 2**: define a local feature flag
    ```go
    chefFeatXYZ := featflag.New("CHEF_FEAT_XYZ", "xyz")
    if chefFeatXYZ.Enabled() {
      // the XYZ feature is enabled, act upon it
    }
    ```
    
    Contributes to: https://github.com/chef/chef-workstation/issues/609
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- pkg/featflag/features_test.go
+++ pkg/featflag/features_test.go
@@ -17,28 +17,81 @@
 package featflag_test
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/chef/chef-analyze/pkg/config"
 	featflag "github.com/chef/chef-analyze/pkg/featflag"
 )
 
-func TestGetFeatureFlag(t *testing.T) {
-	featFlag, exist := featflag.Get("CHEF_FEAT_ALL")
-	if assert.True(t, exist) {
-		assert.Equal(t, featflag.ChefFeatAll, featFlag)
-		assert.Equal(t, "CHEF_FEAT_ALL", featFlag.Name())
-		assert.False(t, featFlag.Enabled())
+func TestDefaultFeatureFlags(t *testing.T) {
+	assert.Equal(t,
+		"(all:CHEF_FEAT_ALL) (analyze:CHEF_FEAT_ANALYZE)",
+		featflag.ListAll(),
+		"default list of features doesn't match",
+	)
+}
+
+func TestGetFromEnvUnknown(t *testing.T) {
+	featFlag, exist := featflag.GetFromEnv("CHEF_FEAT_BAR")
+	if assert.False(t, exist, "unknown feature flag should not exist") {
+		assert.Nil(t, featFlag, "unknown feature flag should return nil")
 	}
 }
 
-func TestGetUnknownFeatureFlag(t *testing.T) {
-	featFlag, exist := featflag.Get("CHEF_FEAT_BAR")
-	if assert.False(t, exist) {
-		assert.Equal(t, featflag.Feature(-1), featFlag)
-		assert.Equal(t, false, featFlag.Enabled())
+func TestGetFromEnvGlobalChefFeatAll(t *testing.T) {
+	featFlag, exist := featflag.GetFromEnv("CHEF_FEAT_ALL")
+	if assert.True(t, exist, "global CHEF_FEAT_ALL:all flag should exist") {
+		assert.True(t,
+			featflag.ChefFeatAll.Equals(featFlag),
+			"the feature flags don't match",
+		)
+		assert.False(t,
+			featFlag.Enabled(),
+			"global feature flag for all features should be disabled",
+		)
+		assert.Equal(t,
+			"CHEF_FEAT_ALL", featFlag.Env(),
+			"environment variable mismatch")
+		assert.Equal(t,
+			"all", featFlag.Key(),
+			"config key mismatch")
+		assert.Equal(t,
+			"(all:CHEF_FEAT_ALL)", featflag.ChefFeatAll.String(),
+			"string conversion mismatch")
+	}
+}
+
+func TestGetFromKeyUnknown(t *testing.T) {
+	featFlag, exist := featflag.GetFromKey("bar")
+	if assert.False(t, exist, "unknown feature flag should not exist") {
+		assert.Nil(t, featFlag, "unknown feature flag should return nil")
+	}
+}
+
+func TestGetFromKeyGlobalChefFeatAll(t *testing.T) {
+	featFlag, exist := featflag.GetFromKey("all")
+	if assert.True(t, exist, "global CHEF_FEAT_ALL:all flag should exist") {
+		assert.True(t,
+			featflag.ChefFeatAll.Equals(featFlag),
+			"the feature flags don't match",
+		)
+		assert.False(t,
+			featFlag.Enabled(),
+			"global feature flag for all features should be disabled",
+		)
+		assert.Equal(t,
+			"CHEF_FEAT_ALL", featFlag.Env(),
+			"environment variable mismatch")
+		assert.Equal(t,
+			"all", featFlag.Key(),
+			"config key mismatch")
+		assert.Equal(t,
+			"(all:CHEF_FEAT_ALL)", featflag.ChefFeatAll.String(),
+			"string conversion mismatch")
 	}
 }
 
@@ -47,7 +100,7 @@ func TestAllFeatureFlagsDisabledByDefault(t *testing.T) {
 	assert.False(t, featflag.ChefFeatAnalyze.Enabled())
 }
 
-func TestEnableChefAnalyzeFeatureFlag(t *testing.T) {
+func TestEnableChefAnalyzeThroughEnvVariable(t *testing.T) {
 	defer os.Setenv("CHEF_FEAT_ANALYZE", "")
 
 	if err := os.Setenv("CHEF_FEAT_ANALYZE", "true"); err != nil {
@@ -56,7 +109,7 @@ func TestEnableChefAnalyzeFeatureFlag(t *testing.T) {
 	assert.True(t, featflag.ChefFeatAnalyze.Enabled())
 }
 
-func TestEnableChefAnalyzeThroughGlobalFeatureFlag(t *testing.T) {
+func TestEnableChefAnalyzeThroughGlobalChefAllFeatureFlag(t *testing.T) {
 	defer os.Setenv("CHEF_FEAT_ALL", "")
 
 	if err := os.Setenv("CHEF_FEAT_ALL", "true"); err != nil {
@@ -66,11 +119,26 @@ func TestEnableChefAnalyzeThroughGlobalFeatureFlag(t *testing.T) {
 }
 
 func TestAddFooFeatureFlag(t *testing.T) {
-	// Defining a new Feature Flag Foo
-	var chefFeatFoo = featflag.New("CHEF_FEAT_FOO")
+	// verify the list of feature flags before adding a new one
+	assert.Equal(t,
+		"(all:CHEF_FEAT_ALL) (analyze:CHEF_FEAT_ANALYZE)",
+		featflag.ListAll(),
+		"default list of features doesn't match",
+	)
 
-	assert.Equal(t, "CHEF_FEAT_FOO", chefFeatFoo.Name())
+	// defining a new Feature Flag Foo
+	var chefFeatFoo = featflag.New("CHEF_FEAT_FOO", "foo")
+
+	assert.Equal(t, "CHEF_FEAT_FOO", chefFeatFoo.Env())
+	assert.Equal(t, "foo", chefFeatFoo.Key())
 	assert.False(t, chefFeatFoo.Enabled())
+	assert.Equal(t,
+		"(all:CHEF_FEAT_ALL) (analyze:CHEF_FEAT_ANALYZE) (foo:CHEF_FEAT_FOO)",
+		featflag.ListAll(),
+		"the new flag should be displayed",
+	)
+
+	defer os.Setenv("CHEF_FEAT_FOO", "")
 
 	if err := os.Setenv("CHEF_FEAT_FOO", "1"); err != nil {
 		t.Fatal("unable to set environment variable")
@@ -78,10 +146,97 @@ func TestAddFooFeatureFlag(t *testing.T) {
 	assert.True(t, chefFeatFoo.Enabled())
 }
 
-func TestUnknownFeatureFlag(t *testing.T) {
-	// An unknown feature flag (that is, not defined with featflag.New())
-	var chefFeatUnknown featflag.Feature = 999
+//
+// tests that involve modifying the config (config.toml)
+//
+func TestAccessingFlagsWithNilConfig(t *testing.T) {
+	featflag.UseConfig(nil)
+	assert.False(t, featflag.ChefFeatAll.Enabled())
+	assert.False(t, featflag.ChefFeatAnalyze.Enabled())
+}
 
-	assert.Equal(t, "", chefFeatUnknown.Name())
-	assert.False(t, chefFeatUnknown.Enabled())
+func TestEnableFlagsFromConfigToml(t *testing.T) {
+	// all flags should be disabled
+	featflag.UseConfig(nil)
+	assert.False(t,
+		featflag.ChefFeatAll.Enabled(),
+		"global ALL feature flag should be disabled",
+	)
+	assert.False(t,
+		featflag.ChefFeatAnalyze.Enabled(),
+		"global ANALYZE feature flag should be disabled",
+	)
+	// even new flags created locally
+	var chefFeatXYZ = featflag.New("CHEF_FEAT_XYZ", "xyz")
+	assert.False(t,
+		chefFeatXYZ.Enabled(),
+		"local XYZ feature flag should be disabled",
+	)
+
+	// initializing a configuration file
+	createConfigToml(t)
+	defer os.RemoveAll(".chef-workstation") // clean up
+
+	// loading the config to the featflag library
+	customConfig, err := config.New()
+	if err != nil {
+		t.Fatal("unable to load config.toml, check why this happened", err.Error())
+	}
+	featflag.UseConfig(&customConfig)
+
+	// now things start getting interesting
+	assert.False(t,
+		featflag.ChefFeatAll.Enabled(),
+		"global ALL feature flag should continue to be disabled",
+	)
+	assert.True(t, // now it is enabled! (from the config)
+		featflag.ChefFeatAnalyze.Enabled(),
+		"global ANALYZE feature flag should be enabled from loaded config.toml",
+	)
+	assert.False(t,
+		chefFeatXYZ.Enabled(),
+		"local XYZ feature flag should continue to be disabled",
+	)
+
+	// since environment variables has more priorityby, by enabling the
+	// global feature flag CHEF_FEAT_ALL all features should be enabled
+	defer os.Setenv("CHEF_FEAT_ALL", "")
+	if err := os.Setenv("CHEF_FEAT_ALL", "gimmeall"); err != nil {
+		t.Fatal("unable to set environment variable")
+	}
+
+	assert.True(t,
+		featflag.ChefFeatAll.Enabled(),
+		"global ALL feature flag should be now enabled",
+	)
+	assert.True(t, // now it is enabled! (from the config)
+		featflag.ChefFeatAnalyze.Enabled(),
+		"global ANALYZE feature flag should continue to be enabled",
+	)
+	assert.True(t,
+		chefFeatXYZ.Enabled(),
+		"local XYZ feature flag should be now enabled",
+	)
+}
+
+// when calling this function, make sure to add the defer clean up as the below example
+// ```go
+// createConfigToml(t)
+// defer os.RemoveAll(".chef-workstation") // clean up
+// ```
+func createConfigToml(t *testing.T) {
+	if err := os.MkdirAll(".chef-workstation", os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	creds := []byte(`
+[features]
+all = false
+analyze = true
+xyz = false
+`)
+	err := ioutil.WriteFile(".chef-workstation/config.toml", creds, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
