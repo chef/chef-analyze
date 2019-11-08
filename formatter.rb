540de0c4ca87f52123edf0104278e663924d3145
Marc A. Paradise <marc.paradise@gmail.com>
2019-11-08 14:27:48 -0500

    Add 'cookbook state' report
    This report queries chef server for all cookbooks in the
    configured organization, then analyzes each of them with chefstyle
    to determine which ones contain errors.
    
    For each cookbook version it reports back:
    * name
    * version
    * number of cookstyle violations
    * number of those that are correctable
    * nodes using this cookbook+version
    
    Future improvements:
    
    - this is highly parallelizable. In future iterations,
      we can structure this so that we start analyzing as soon as
      we download the first complete cookbook, and fan out to N active
      goroutines to continue concurrent download/analysis
    - error handling - which errors do we care about, and which ones
                       does the human operator need to know about (eg, which
                       ones are actionable by the human operator)
    - progress updates - this behavior should be living in the report
                         command, which is responsible for the TUI; but we
                         need to define what that looks like across
                         commands.
    
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

---
+++ formatter.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+module Local
+  # A simple formatter that outputs the total number of violations
+  # found, and the total number of those that are auto-correctable.
+  class CorrectableCountFormatter < RuboCop::Formatter::BaseFormatter
+    def initialize(output, options = {})
+      super
+      @offense_count = 0
+      @correctable_count = 0
+      @registry = RuboCop::Cop::Registry.new(RuboCop::Cop::Cop.all)
+    end
+
+
+    def file_finished(_file, offenses)
+      @offense_count += offenses.count
+      offenses.each do |offense|
+        # If a cop has an autocorrect method (inherited or otherwise) then it can
+        # be autocorrected:
+        @correctable_count += 1 if @registry.find_by_cop_name(offense.cop_name).method_defined?(:autocorrect)
+      end
+    end
+
+    def finished(_inspected_files)
+      output.write "#{@offense_count} #{@correctable_count}"
+    end
+  end
+end
