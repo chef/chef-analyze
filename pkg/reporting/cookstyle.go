2777fd6821cb288d2aeac1d52ca71a31c4712397
Marc A. Paradise <marc.paradise@gmail.com>
2019-11-19 17:02:43 -0500

    Move cookstyle handling to be shareable
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

---
+++ pkg/reporting/cookstyle.go
@@ -0,0 +1,58 @@
+package reporting
+
+import (
+	"encoding/json"
+	"os/exec"
+)
+
+type CookstyleResult struct {
+	Severity    string `json:severity`
+	Message     string `json:message`
+	CopName     string `json:"cop_name"`
+	Corrected   bool   `json:"corrected"`
+	Correctable bool   `json:"correctable"`
+	Location    struct {
+		StartLine   int `json:"start_line"`
+		StartColumn int `json:"start_column"`
+		LastLine    int `json:"last_line"`
+		LastColumn  int `json:"last_column"`
+		Length      int `json:"length"`
+		Line        int `json:"line"`
+		Column      int `json:"column"`
+	}
+}
+
+// Should these live here?
+type CommandRunner interface {
+	Run(workingDir string, name string, arg ...string) ([]byte, error)
+}
+
+type ExecCommandRunner struct{}
+
+func (er ExecCommandRunner) Run(workingDir string, name string, arg ...string) ([]byte, error) {
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = workingDir
+	return cmd.Output()
+}
+
+func RunCookstyle(workingDir string, runner CommandRunner) ([]CookstyleResult, error) {
+	// TODO - this will only work on *nix flavors - platform resolution of binaries is a shared thing we need.
+	output, err := runner.Run(workingDir, "/opt/chef-workstation/bin/cookstyle", "--format", "json")
+	if exitError, ok := err.(*exec.ExitError); ok {
+		// https://docs.rubocop.org/en/latest/basic_usage/#exit-codes
+		// exit code of 1 is ok , it means some violations were found
+		if exitError.ExitCode() != 1 {
+			return nil, err
+		}
+	} else if err != nil {
+		return nil, err
+	}
+	var csr []CookstyleResult
+
+	err = json.Unmarshal(output, &csr)
+	if err != nil {
+		return nil, err
+	}
+
+	return csr, nil
+}
