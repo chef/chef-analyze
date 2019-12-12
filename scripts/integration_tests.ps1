d9b18c2a420c2c3054ca08f644934b34bbfe9df8
Salim Afiune <afiune@chef.io>
2019-12-12 18:01:39 +0100

    integration:(fix-test) catch errors on buildkite
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- scripts/integration_tests.ps1
+++ scripts/integration_tests.ps1
@@ -1,9 +1,18 @@
+# Stop script execution when a non-terminating error occurs
+$ErrorActionPreference = "Stop"
+
+# Enable the use of Go vendor/ directory
+$Env:GOFLAGS = "-mod=vendor"
+
 ECHO "-- Installing Golang"
 hab pkg install -b afiune/go
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
 
 ECHO "-- Building chef-analyze EXE"
 go build -o bin\chef-analyze_windows.exe
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
 
 ECHO "-- Running integration tests (github.com/chef/chef-analyze/integration)"
 $Env:CHEF_ANALYZE_BIN = (Get-PSDrive Habitat).Root + "\src\bin\chef-analyze_windows.exe"
 go test github.com/chef/chef-analyze/integration -v
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
